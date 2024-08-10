77a0cec3954ce7bbbe3525872084bfaee10107e3
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-08-10 08:53:14 +0700

    Feat: create script to fill all radio buttons

---
+++ script.js
@@ -0,0 +1,47 @@
+// Select all radio input elements with type="radio" and value="Ada"
+document
+  .querySelectorAll('input[type="radio"][value="Ada"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
+
+// Select all radio input elements with type="radio" and value="Ya"
+document
+  .querySelectorAll('input[type="radio"][value="Ya"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
+
+// Select all radio input elements with type="radio" and value="Wifi Kampus"
+document
+  .querySelectorAll('input[type="radio"][value="Wifi Kampus"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
+
+// Select all radio input elements with type="radio" and value="Laptop"
+document
+  .querySelectorAll('input[type="radio"][value="Laptop"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
+
+// Select all radio input elements with type="radio" and value="Tatap Muka"
+document
+  .querySelectorAll('input[type="radio"][value="Tatap Muka"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
+
+// Select all radio input elements with type="radio" and value="Ms Teams"
+document
+  .querySelectorAll('input[type="radio"][value="Ms Teams"]')
+  .forEach(function (radio) {
+    // Set the 'checked' property of each radio button to true, selecting it
+    radio.checked = true;
+  });
